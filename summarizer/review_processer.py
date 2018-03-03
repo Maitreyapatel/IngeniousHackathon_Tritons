82f5c11a250ac65eb5a4bdb1078c775420deb415
Maitreyapatel <patel.maitreya57@gmail.com>
2018-03-04 00:41:12 +0530

    Example reviews file and review processer

---
+++ summarizer/review_processer.py
@@ -0,0 +1,3 @@
+wordsForDelete = ["bought","what","how","when","where","why","we","we've","i've","buck","bucks","dollars","dollar","home","office","customer","price","budget","fix","i","my","happy","day","overall","days","got","get","delivered","deliver","she","you","u","he","am","mine"]
+
+#print(len(word))
